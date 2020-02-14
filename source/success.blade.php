3649801f6bb220e88db403f9148455aaa0b4c994
Max <hello@max.berlin>
2020-02-14 20:18:18 +0100

    Fix: swap mail ico with checkmark

--- source/success.blade.php
+++ source/success.blade.php
@@ -8,12 +8,12 @@
 
 <div class="pt-32">
   <div class="flex justify-center">
-    <span class="block px-4 py-2"><img class="h-20 m-auto" src="/assets/images/mail-humba.svg" alt="Email Icon"></span>
+    <span class="block px-4 py-2"><img class="h-40 m-auto" src="/assets/images/checkmark.svg" alt="Email Icon"></span>
   </div>
 
   <span class="block text-humba-pink font-bold   text-4xl text-center px-4 py-2 mt-2"<p> Woah, so einfach. </p></span>
-  <p class="text-red-400 text-center" >In den folgenden Wochen bekommst du eine Email von uns!</p> </span>
-  <span class="block text-humba-pink text-sm font-thin text-center pb-8 px-4 mt-2"><p> Du hast Fragen oder Anregungen? Dann schreibe uns an support@hu.mba. </p> </span>
+  <p class="text-red-400 font-semibold text-center" >In den folgenden Wochen bekommst du eine Email von uns!</p> </span>
+  <span class="block text-red-200 text-sm font-thin text-center pb-8 px-4 mt-2"><p> Du hast Fragen oder Anregungen? Dann schreibe uns an support@hu.mba. </p> </span>
 </div>
 
 @endsection
