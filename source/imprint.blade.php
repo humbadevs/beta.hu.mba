9fc3390a6d33a749af7ae3229fcb7977a6a6c8aa
Max <hello@max.berlin>
2020-02-14 19:11:30 +0100

    Add: imprint

---
+++ source/imprint.blade.php
@@ -0,0 +1,30 @@
+
+@extends('_layouts.process')
+
+@section('content')
+<div class="px-16 pt-20">
+  <a href="/"><img class="h-10" src="/assets/images/logo.png" alt="Logo"></a>
+</div>
+
+<div class="m-8 text-humba-pink">
+  <h1 class="mt-8 text-5xl text-humba-pink font-bold">Imprint</h1>
+  <div class="opacity-50">
+    <p class="font-bold mt-8 mb-4">Angaben gemäß § 5 TMG:</p>
+    <ul class="list-none">
+      <li>Humba Network</li>
+      <li>Lohmühlenstr. 65</li>
+      <li>12435 Berlin</li>
+    </ul>
+    <p class="font-bold my-4">Vertreten durch:</p>
+    <div class="">
+      <a href="https://max.berlin/about" class="underline">Maximilian Jendrall</a>
+    </div>
+    <p class="font-bold my-4">Kontakt:</p>
+    <ul class="list-none">
+      <li>Telefon: 030 36 46 80 48</li>
+      <li>E-Mail: legal (at) max.berlin</li>
+    </ul>
+  </div>
+</div>
+
+@endsection
