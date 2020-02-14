315a175b8cab4eeaf159f64da9eabd01dcdb01e3
Max <hello@max.berlin>
2020-02-14 20:38:50 +0100

    Add: privacy policy

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="mt-24 flex flex-wrap justify-center sm:justify-between">
   <div class="ml-3 mb-3 static  xl:bottom-0 text-humba-pink text-xs">&copy; 2020 humba team - All rights reserved.</div>
   <div class="text-right static xl:right-0 xl:bottom-0 mb-3 ml-3 md:mr-3 text-humba-pink text-xs">
-    <a href="/imprint">Impressum</a>, <a href="/data">Datenschutz</a>
+    <a href="/imprint">Impressum</a>, <a href="https://www.notion.so/humbamanagement/Privacy-b94679c757fb4ddba98be32908a8a412">Datenschutz</a>
   </div>
 </footer>
