a86ca5e4feefc42dd47f08b1f7336e10957d62ea
Max <hello@max.berlin>
2020-02-14 19:18:41 +0100

    Fix: position of footer text

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="mt-24 flex flex-wrap justify-center sm:justify-between">
   <div class="ml-3 mb-3 static  xl:bottom-0 text-humba-pink text-xs">&copy; 2020 humba team - All rights reserved.</div>
-  <div class="text-right static xl:right-0 xl:bottom-0 mb-3 mr-3 text-humba-pink text-xs">
+  <div class="text-right static xl:right-0 xl:bottom-0 mb-3 ml-3 md:mr-3 text-humba-pink text-xs">
     <a href="/imprint">Impressum</a>, <a href="/data">Datenschutz</a>
   </div>
 </footer>
