ca46a470817ec5b60181b8b8a6b19073166f4a81
Max <hello@max.berlin>
2020-04-16 17:58:57 +0200

    Add: v1.1 texts for digital launch

--- source/index.blade.php
+++ source/index.blade.php
@@ -7,7 +7,7 @@
           <div class="flex justify-center md:block">
             <img class="h-12 md:h-auto" src="/assets/images/logo-white-full.svg" alt="">
           </div>
-          <p class="font-extrabold text-white pt-6 text-s">humba ist die App mit der du deine Freistunde oder Pause endlich sinnvoller verbringen kannst. Suche einfach andere, die auch frei haben, lerne neue Menschen kennen, lass dir von uns Aktivitäten vorschlagen oder gehe schon bald günstiger bei unseren Partnern essen.</p>
+          <p class="font-extrabold text-white pt-6 text-s">humba ist die App mit der du dich mit den anderen Menschen aus deiner Schule auf eine leichte Art und Weise verbinden kannst. Digital, easy und spaßig.</p>
         </div>
         <div class="md:w-4/6 xxl:w-3/4">
           <h1 class="text-right text-white font-extrabold text-6xl xxl:text-7xl">Deine freie Zeit<br> ist wertvoll.<br> Nutze sie.</h1>
@@ -16,7 +16,7 @@
       <div class="bg-humba-pink md:hidden text-white pt-8">
         <span class="block"> <a href="/"><img class="m-auto h-12" src="/assets/images/logo-white-full.svg" alt="Logo"></a> </span>
         <span class="block text-center font-bold pt-10 text-5xl m-8"> Deine freie Zeit ist wertvoll. Nutze sie.</span>
-        <span class="block text-center m-6 pb-20">humba ist die App mit der du deine Freistunde oder Pause endlich sinnvoller verbringen kannst. Suche einfach andere, die auch frei haben, lerne neue Menschen kennen, lass dir von uns Aktivitäten vorschlagen oder gehe schon bald günstiger bei unseren Partnern essen.</span>
+        <span class="block text-center m-6 pb-20">humba ist die App mit der du dich mit den anderen Menschen aus deiner Schule auf eine leichte Art und Weise verbinden kannst. Digital, easy und spaßig.</span>
       </div>
       <div class="pt-20">
         <h2 class="text-center font-bold text-1xl px-4 md:text-3xl text-humba-pink">Jetzt für die Beta anmelden <br>und bald die erste Version testen.</h2>
